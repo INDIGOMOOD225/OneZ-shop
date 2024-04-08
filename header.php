269311a317dea80a069d29f4bd3318740942557a
INDIGOMOOD225 <160881740+INDIGOMOOD225@users.noreply.github.com>
2024-04-08 19:57:53 +0200

    graphical rework to header and footer

--- header.php
+++ header.php
@@ -7,41 +7,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 <head>
     <title>My Website</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <style>
-        /* Add CSS styles for graphical elements */
-        header {
-            background-color: #f2f2f2;
-            padding: 20px;
-            text-align: center;
-        }
-
-        h1 {
-            color: #333;
-            font-size: 32px;
-        }
-
-        nav ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-        }
-
-        nav ul li {
-            margin: 0 10px;
-        }
-
-        nav ul li a {
-            text-decoration: none;
-            color: #333;
-            font-weight: bold;
-        }
-
-        nav ul li a:hover {
-            color: #ff0000;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="header.css">
 </head>
 <body>
     <header>
