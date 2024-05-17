bf869a627f6cd3675c52acf7e336363b39556c44
INDIGOMOOD225 <160881740+INDIGOMOOD225@users.noreply.github.com>
2024-05-17 10:43:28 +0200

    header and footer izolovane

---
+++ parts/footer.php
@@ -0,0 +1,45 @@
+<?php
+include "parts/footer.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rogue´s Lair</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container-fluid">
+    <footer>
+        <div class="row">
+        <div class="col-sm-10 col-md-4 col-lg-4"></div>
+        <div class="col-sm-10 col-md-3 col-lg-5">
+        <div class="image-container">
+          <img src="1rogue.gif" width="300" height="150" position: center;>
+        </div>
+        </div>
+        <div class="col-sm-12 col-md-5 col-lg-5"></div>
+       </div>
+        <div class="content">
+          <p>
+            <span class="author">Teo Burcl </span>creator and founder of this site. Huge fan of <em>Old school Rogue-likes</em>. This site is tribute to allmighty games of the same genre <em>praise the permadeath concept</em>
+          </p>
+          <p>
+            All rights reserved <strong>© 2021</strong> <br> <br><button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" title="Last Warning!!!">
+      Do not touch!</button><br>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-primary" target="_blank" rel="noopener noreferrer">Try this instead!</a>
+          </p>
+
+
+          <!-- adresa -->
+          <address>
+    Written by <a href="mailto:teo.burcl@ukf.sk" style="text-decoration:none">Teo Burcl</a>.<br>
+    Visit us at:<br>
+
+    Tralaland, 949 01  Nitra<br>
+    Slovakia
+    </address>
+        </div>
+      </footer>
+    </div>
+</body>
+</html>
---
+++ parts/header.php
@@ -0,0 +1,41 @@
+<?php
+include "parts/header.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rogue´s Lair</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+/* internal css */
+    a { text-decoration: none; }
+    header s{color: black;}
+  </style>
+</head>
+<body>
+    <!-- Header {navbar,class, inline css}-->
+  <nav class="header">
+    <span class="logo"><a href="index.html" style="text-decoration:none";>ROGUE'S L@IR</a></span>
+    <ul>
+      <li><a href="about.html">@bout</a></li>
+      <li><a href="visitors.html">Visitors</a></li>
+  <li><a href="faq.html">Subscribe</a></li>
+    </ul>
+
+  </nav>
+  <div class="searchbar">
+   <div class="container">
+       <form>
+           <span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span><input type="text" id="Form_Search" value="" placeholder="SE@RCH THE L@IR" role="searchbox" class="InputBox " autocomplete="off"><input type="submit" id="Form_Go" class="Button" value="GO">
+       </form>
+   </div>
+  </div>
+
+  <!-- Banner {idcka, class, img, link, }-->
+  <div class="banner" style="background:url('https://free4kwallpapers.com/uploads/originals/2019/04/16/futuristic-city-wallpaper.jpg');background-repeat:no-repeat;background-size: cover">
+    <h1>Rogue's L@ir</h1>
+    <h2>carefully crafted with passion and love by Teo</h2>
+
+  </div>
+</body>
+</html>
