e76082d0c5bd5b3683bdcd442462dd4e477fdc2b
INDIGOMOOD225 <160881740+INDIGOMOOD225@users.noreply.github.com>
2024-05-17 10:09:36 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-$author = "Teo Burcl";
-$year = date("Y");
-?>
-
-<footer>
-    <p>&copy; <?php echo $year; ?> Ramen2Go. All rights reserved. Created by <?php echo $author; ?></p>
-</footer>
-<link rel="stylesheet" href="path/to/your/footer.css">
